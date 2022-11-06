299e481af98060284f4430419992c8dcffead03d
Asjal Abrar <asjalabrar8@gmail.com>
2022-11-06 18:29:09 +0500

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Hero from "./component/Hero/Hero";
+import Plans from "./component/Plans/Plans";
 import Programs from "./component/Programs/Programs";
+import Reasons from "./component/Reasons/Reasons";
 function App() {
   return (
     <div className="App">
       <Hero />
       <Programs />
+      <Reasons />
+      <Plans />
     </div>
   );
 }
---
+++ src/component/Plans/Plans.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Plans.css";
+
+const Plans = () => {
+  return (
+    <div>
+      <div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Plans;
